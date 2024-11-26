013eb48f7fd87651731561515bc75d52428dd37c
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2024-11-26 13:40:29 +0300

    Update Alive.js

--- lazackcmds/Alive.js
+++ lazackcmds/Alive.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let name = m.pushName || conn.getName(m.sender)
   var vn = 'https://cdn.jsdelivr.net/gh/SilvaTechB/silva-md-bot@main/media/Alive.mp3'
   let url = 'https://github.com/SilvaTechB/silva-md-bot'
-  let murl = 'https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v'
-  let img = 'https://files.catbox.moe/8324jm.jpg'
+  let murl = 'https://chat.whatsapp.com/CAKGysbedAWCrGmrm5bOiz'
+  let img = 'https://i.imgur.com/gcB7GLV.jpeg'
   let con = {
     key: {
       fromMe: false,
@@ -30,10 +30,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓 Alive',
-        body: 'SILVA MD BOT OCTOBER 2024',
+        title: 'MICKEY™',
+        body: 'I AM MICKEY HOW ARE YOU',
         thumbnailUrl: img,
-        sourceUrl: 'https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v',
+        sourceUrl: 'https://chat.whatsapp.com/CAKGysbedAWCrGmrm5bOiz',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
